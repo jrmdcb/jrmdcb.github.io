64805b8ade0e51b5e0a37870cbb41b957dcb3ce8
jeremy <jrmdcb@gmail.com>
2024-07-28 20:25:06 +0800

    [App Routes] Added projects, about, and contact route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ProjectsComponent } from './modules/projects/projects.component';
+import { AboutComponent } from './modules/about/about.component';
+import { ContactComponent } from './modules/contact/contact.component';
+
+export const routes: Routes = [
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+];
